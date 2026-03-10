77cf0c8b3aeaa7f6c7dfd0a6b9be4a0788d3d580
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2026-03-10 19:17:46 +0100

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,7 +8,20 @@ export class Counter extends Component {
   addValue = () => {
     // this.state.counterData =  +1 // так змінювати стaн НЕ можна
     //   для зміни стану використовують метод this.setState()
-    this.setState({ counterData: 5 });
+    // this.setState({ counterData: 5 }); в цей метод передаємо новий об'єкт, якщо треба замінити значення на нове, не прив'язуючись до попереднього значення
+    // якщо потрібно отримати нове значення, використовуючи попереднє значення, то передаємо в setState(callback)
+    this.setState((currentState) => {
+      return {
+        counterData: currentState.counterData + 1,
+      };
+    }); // зробили стрілку з явним поверненням об'єкту
+  };
+
+  decreaseValue = () => {
+    // this.setState({ counterData: 0 });
+    this.setState((currentState) => ({
+      counterData: currentState.counterData - 1,
+    })); // зробили стрілку з неявним поверненням об'єкту
   };
 
   render() {
@@ -16,7 +29,7 @@ export class Counter extends Component {
       <div>
         <p>Counter Value: {this.state.counterData}</p>
         <button onClick={this.addValue}>Increase value</button>
-        <button>Decrease value</button>
+        <button onClick={this.decreaseValue}>Decrease value</button>
       </div>
     );
   }
